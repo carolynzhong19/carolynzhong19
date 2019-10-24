6d1ddc058b98026862e42857f13b392a8991f7c5
Carolyn Zhong <carolynz@andrew.cmu.edu>
2019-10-23 23:57:24 -0400

    choose glaze

--- assgn6/main.js
+++ assgn6/main.js
@@ -1,3 +1,13 @@
 function addtocart() {
 	document.getElementById('cart').src='https://i.ibb.co/TTCWwG8/box1.png'
 }
+
+function chooseglaze() {
+	var x = document.getElementById("glaze").value;
+	if (x == "none") {
+		document.getElementById("cinnaroll").src= 'https://i.ibb.co/DL7QkWd/noglaze.png';
+	}
+	if  (x == "double chocolate") {
+		document.getElementById("cinnaroll").src= 'https://i.ibb.co/FH8PJFn/chocolate.png';
+	}
+}
