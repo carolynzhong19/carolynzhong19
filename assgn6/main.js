f2e91e7d920f49cfe399bab208a1dcabb7878ed8
Carolyn Zhong <carolynz@andrew.cmu.edu>
2019-10-28 20:49:49 -0400

    js choose glaze

--- assgn6/main.js
+++ assgn6/main.js
@@ -7,6 +7,12 @@ function chooseglaze() {
 	if (x == "none") {
 		document.getElementById('cinnaroll').src= 'https://i.ibb.co/DL7QkWd/noglaze.png';
 	}
+	if (x == "sugar milk") {
+		document.getElementById('cinnaroll').src= 'https://i.ibb.co/09v1H1S/psroll.png';
+	}
+	if (x == "vanilla milk") {
+		document.getElementById('cinnaroll').src= 'https://i.ibb.co/09v1H1S/psroll.png';
+	}
 	if  (x == "double chocolate") {
 		document.getElementById('cinnaroll').src= 'https://i.ibb.co/FH8PJFn/chocolate.png';
 	}
